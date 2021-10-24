6538627739aece143aaceda2cd0f9b27f5772764
Akhila Srinagula <89111745+AkhilaSrinagula@users.noreply.github.com>
2021-10-24 04:59:14 -0500

    added timings

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,82 +1,100 @@
 var today = moment().format("dddd MMMM Do, YYYY");
 var liveTime = moment().format("HH");
-var hours = ["09", "10", "11", "12", "01", "02", "03", "04", "05", "06"];
+var hours = ["09", "10", "11", "12", "13", "14", "15", "16", "17", "18"];
 
 //Setting current Date in Jumbotron
 $("#currentDay").html(today);
 
-//setItem and getItem from local storage
-$(".09").on("click", function () {
-  var description = $(".09").val();
-  localStorage.setItem("09:00", description);
-});
-$(".09").val(localStorage.getItem("09:00"));
 
-$(".10").on("click", function () {
-  var description = $(".10").val();
-  localStorage.setItem("10:00", description);
-});
-$(".10").val(localStorage.getItem("10:00"));
+//setItem and getItem from local storage 
+$('.09').on("click", function () {
+    var description = $('.09').val();
+    localStorage.setItem("09:00", description);
+  });
+  $('.09').val(localStorage.getItem("09:00"));
 
-$(".11").on("click", function () {
-  var description = $(".11").val();
-  localStorage.setItem("11:00", description);
-});
-$(".11").val(localStorage.getItem("11:00"));
+ $('.10').on("click", function () {
+    var description = $('.10').val();
+    localStorage.setItem("10:00", description);
+  });
+  $('.10').val(localStorage.getItem("10:00"));
 
-$(".12").on("click", function () {
-  var description = $(".12").val();
-  localStorage.setItem("12:00", description);
-});
-$(".12").val(localStorage.getItem("12:00"));
 
-$(".01").on("click", function () {
-  var description = $(".01").val();
-  localStorage.setItem("01:00", description);
-});
-$(".01").val(localStorage.getItem("01:00"));
+  $('.11').on("click", function () {
+    var description = $('.11').val();
+    localStorage.setItem("11:00", description);
+  });
+  $('.11').val(localStorage.getItem("11:00"));
 
-$(".02").on("click", function () {
-  var description = $(".02").val();
-  localStorage.setItem("02:00", description);
-});
-$(".02").val(localStorage.getItem("02:00"));
 
-$(".03").on("click", function () {
-  var description = $(".03").val();
-  localStorage.setItem("03:00", description);
-});
-$(".03").val(localStorage.getItem("03:00"));
+  $('.12').on("click", function () {
+    var description = $('.12').val();
+    localStorage.setItem("12:00", description);
+  });
+  $('.12').val(localStorage.getItem("12:00"));
 
-$(".04").on("click", function () {
-  var description = $(".04").val();
-  localStorage.setItem("04:00", description);
-});
-$(".04").val(localStorage.getItem("04:00"));
 
-$(".05").on("click", function () {
-  var description = $(".05").val();
-  localStorage.setItem("05:00", description);
-});
-$(".05").val(localStorage.getItem("05:00"));
+  $('.13').on("click", function () {
+    var description = $('.13').val();
+    localStorage.setItem("13:00", description);
+  });
+  $('.13').val(localStorage.getItem("13:00"));
 
-$(".06").on("click", function () {
-  var description = $(".06").val();
-  localStorage.setItem("06:00", description);
-});
-$(".06").val(localStorage.getItem("06:00"));
+
+  $('.14').on("click", function () {
+    var description = $('.14').val();
+    localStorage.setItem("14:00", description);
+  });
+  $('.14').val(localStorage.getItem("14:00"));
+
+
+
+  $('.15').on("click", function () {
+    var description = $('.15').val();
+    localStorage.setItem("15:00", description);
+  });
+  $('.15').val(localStorage.getItem("15:00"));
+
+
+
+  $('.16').on("click", function () {
+    var description = $('.16').val();
+    localStorage.setItem("16:00", description);
+  });
+  $('.16').val(localStorage.getItem("16:00"));
+
+
+
+  $('.17').on("click", function () {
+    var description = $('.17').val();
+    localStorage.setItem("17:00", description);
+  });
+  $('.17').val(localStorage.getItem("17:00"));
+
+
+
+  $('.18').on("click", function () {
+    var description = $('.18').val();
+    localStorage.setItem("18:00", description);
+  });
+  $('.18').val(localStorage.getItem("18:00"));
+
+
 
 function timeTracker() {
-  for (var i = 0; i < hours.length; i++) {
-    if (hours[i] < liveTime) {
-      $("." + hours[i]).addClass("past");
+    for (var i = 0; i < hours.length; i++) {
+        if (hours[i] < liveTime) {
+            $('.' + hours[i]).addClass("past");
+        };
+        if (hours[i] === liveTime) {
+            $('.' + hours[i]).addClass("present");
+        };
+        if (hours[i] > liveTime) {
+            $('.' + hours[i]).addClass("future");
+        };
     }
-    if (hours[i] === liveTime) {
-      $("." + hours[i]).addClass("present");
-    }
-    if (hours[i] > liveTime) {
-      $("." + hours[i]).addClass("future");
-    }
-  }
 }
 timeTracker();
+
+
+
